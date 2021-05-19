faa26a3bb43360a5233cb23ed84be05b9a3dbc69
ROGERIO RAMOS BARROS FERREIRA <rogerio.ferreira@bradesco.com.br>
2021-05-19 11:26:11 -0300

    FIX problems config

--- src/main.rs
+++ src/main.rs
@@ -121,7 +121,7 @@ async fn main() -> std::io::Result<()> {
                 .route(web::get().to(get_key)),
         )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
